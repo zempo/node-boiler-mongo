a28670776ed3cd7784fbf9f820760c92154f7765
zempo <zelenkosolomon@gmail.com>
2020-03-28 16:23:46 -0700

    add mongo sanitizer

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const mongoSanitize = require("express-mongo-sanitize");
 const { NODE_ENV } = require("./config/globals");
 
 const app = express();
@@ -15,6 +16,11 @@ app.use(cors());
 const morganSettings = NODE_ENV === "production" ? "tiny" : "dev";
 app.use(morgan(morganSettings));
 app.use(helmet());
+app.use(
+  mongoSanitize({
+    replaceWith: "_"
+  })
+);
 // body parser
 app.use(express.json({ extended: false }));
 //
